dcf99f27287c2b8990a7228329e21ecd2b88497b
PJLys <pjlyssens@gmail.com>
2022-11-17 16:31:37 +0100

    Writing documentation in the database package; moving initiation into the constructor of PersonDB

--- code/src/main/databases/PersonDB.java
+++ code/src/main/databases/PersonDB.java
@@ -1,13 +1,23 @@
 package databases;
 
 import person.Person;
-
 import java.util.LinkedList;
+
+/**
+ * Person DB
+ * - Singleton (private constructor & public getInstance())
+ */
 
 public class PersonDB {
     private PersonDB instance = null;
-    private LinkedList<Person> db = new LinkedList<Person>();
-    private PersonDB(){}
+    /**
+     * DB is a LinkedList : easy addition & removal of Persons
+     */
+    private LinkedList<Person> db;
+
+    private PersonDB(){
+        this.db = new LinkedList<>();
+    }
 
     public PersonDB getInstance() {
         if (instance==null)
@@ -15,6 +25,10 @@ public class PersonDB {
         return this.instance;
     }
 
+    /**
+     * @param p person to be added
+     * @return 0 = succes; -1 = t is a duplicate
+     */
     public int addPerson(Person p){
         if (!this.db.contains(p)){
             this.db.add(p);
@@ -23,6 +37,10 @@ public class PersonDB {
         return -1;
     }
 
+    /**
+     * @param p person to be removed
+     * @return 0 = succes; -1 = p isn't in the db
+     */
     public int removePerson(Person p){
         if (this.db.contains(p)){
             this.db.remove(p);
--- code/src/main/databases/TicketDB.java
+++ code/src/main/databases/TicketDB.java
@@ -1,15 +1,24 @@
 package databases;
 
 import tickets.Ticket;
+import java.util.LinkedList;
 
-import java.util.ArrayList;
 
+/**
+ * Ticket DB
+ * - Singleton (private constructor & public getInstance())
+ */
 public class TicketDB {
+
     private TicketDB instance = null;
-    private ArrayList<Ticket> db;
+
+    /**
+     * DB is a Linked List : easy addition & removal of Tickets
+     */
+    private LinkedList<Ticket> db;
 
     private TicketDB(){
-        this.db = new ArrayList<>();
+        this.db = new LinkedList<>();
     }
 
     public TicketDB getInstance() {
@@ -18,6 +27,10 @@ public class TicketDB {
         return instance;
     }
 
+    /**
+     * @param t ticket to be added
+     * @return 0 = succes; -1 = t is a duplicate
+     */
     public int addTicket(Ticket t){
         if (!this.db.contains(t)){
             this.db.add(t);
@@ -26,6 +39,10 @@ public class TicketDB {
         return -1;
     }
 
+    /**
+     * @param t ticket to be removed
+     * @return 0 = succes; -1 = t isn't in the db
+     */
     public int removeTicket(Ticket t){
         if (this.db.contains(t)){
             this.db.remove(t);
