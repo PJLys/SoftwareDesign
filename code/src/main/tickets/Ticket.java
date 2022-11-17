454ceb85553a7d23e152baa5da59b0c6661f8339
PJLys <pjlyssens@gmail.com>
2022-11-17 17:42:51 +0100

    Ticket attributes, constructor and helper types

--- code/src/main/tickets/Ticket.java
+++ code/src/main/tickets/Ticket.java
@@ -1,4 +1,20 @@
 package tickets;
 
+import person.Person;
+
+enum ExpenseType {
+    AIRPLANE,
+    RESTAURANT,
+    TAXI,
+    CONCERT,
+    HOTEL
+}
+
 public abstract class Ticket {
+    private ExpenseType type;
+    private Person payer;
+    public Ticket(ExpenseType t, Person p){
+        this.type = t;
+        this.payer = p;
+    }
 }
