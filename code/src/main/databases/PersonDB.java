8047b172526b6b7eb0cb40f19675bb3dbf2bd4e9
PJLys <pjlyssens@gmail.com>
2022-11-17 16:18:27 +0100

    Writing functions in PersonDB.java

--- code/src/main/databases/PersonDB.java
+++ code/src/main/databases/PersonDB.java
@@ -1,4 +1,33 @@
 package databases;
 
+import person.Person;
+
+import java.util.LinkedList;
+
 public class PersonDB {
+    private PersonDB instance = null;
+    private LinkedList<Person> db = new LinkedList<Person>();
+    private PersonDB(){}
+
+    public PersonDB getInstance() {
+        if (instance==null)
+            this.instance = new PersonDB();
+        return this.instance;
+    }
+
+    public int addPerson(Person p){
+        if (!this.db.contains(p)){
+            this.db.add(p);
+            return 0;
+        }
+        return -1;
+    }
+
+    public int removePerson(Person p){
+        if (this.db.contains(p)){
+            this.db.remove(p);
+            return 0;
+        }
+        return -1;
+    }
 }
