bc79e002b3647f961451f7210c4cd3bb5b437ae6
PJLys <pjlyssens@gmail.com>
2022-11-17 16:23:16 +0100

    Writing functions in TicketDB.java

--- code/src/main/databases/TicketDB.java
+++ code/src/main/databases/TicketDB.java
@@ -1,4 +1,36 @@
 package databases;
 
+import tickets.Ticket;
+
+import java.util.ArrayList;
+
 public class TicketDB {
+    private TicketDB instance = null;
+    private ArrayList<Ticket> db;
+
+    private TicketDB(){
+        this.db = new ArrayList<>();
+    }
+
+    public TicketDB getInstance() {
+        if (instance == null)
+            this.instance = new TicketDB();
+        return instance;
+    }
+
+    public int addTicket(Ticket t){
+        if (!this.db.contains(t)){
+            this.db.add(t);
+            return 0;
+        }
+        return -1;
+    }
+
+    public int removeTicket(Ticket t){
+        if (this.db.contains(t)){
+            this.db.remove(t);
+            return 0;
+        }
+        return -1;
+    }
 }
