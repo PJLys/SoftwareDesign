020640b8f5e05167093f8d61f4505b081d78a4f1
PJLys <pjlyssens@gmail.com>
2022-12-01 12:17:23 +0100

    Iterator pattern

---
+++ code/src/main/databases/TicketIt.java
@@ -0,0 +1,26 @@
+package databases;
+
+import iterator.Iterator;
+
+public class TicketIt implements Iterator {
+    private int index;
+    public TicketIt() {
+
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return TicketDB.getInstance().get(index++);
+    }
+
+    @Override
+    public int remove() {
+        TicketDB db = TicketDB.getInstance();
+        return db.removeTicket(db.get(this.index));
+    }
+}
